6548d6c337e6d96c2e896639e27286e2fe21787a
Jarnrsvlnn <173128640+Jarnrsvlnn@users.noreply.github.com>
2025-09-16 20:11:50 +0800

    added some minor changes

--- app/App.php
+++ app/App.php
@@ -2,4 +2,19 @@
 
 declare(strict_types = 1);
 
-// Your Code
+/** OBJECIVES
+ * 1. Create a function that is able to read
+ * every csv file in the transaction_files directory
+ * 2. Store those collected data inside an array
+ */
+
+// function to get the csv file and return it as an array
+
+function getTransactionFile(string $filename): array {
+    foreach(scandir(FILES_PATH) as $file) {
+        if (is_file($file)) {
+            
+        }
+    }
+}
+
